2e8171e0b02506f4558126737a316340e74d20e7
Lê Chí Luân (Brian Lee) <chiluan6601@gmail.com>
2023-08-21 19:00:28 +0700

    Update server

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-require('dotenv').config();
+const https = require('https');
+const fs = require('fs');
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const connection = require('./database');
+
+const app = express();
+
+// Load your self-signed certificate files
+const privateKey = fs.readFileSync('key.pem', 'utf8');
+const certificate = fs.readFileSync('cert.pem', 'utf8');
+
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+};
 
 // routes
 const userRoutes = require("./routes/users");
@@ -17,12 +28,14 @@ app.use(express.json());
 app.use(cors());
 
 // routes
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 443; // Change to 443 for HTTPS
 app.use("/", todoRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-    }
-);
+// Create an HTTPS server using the self-signed certificate
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, () => {
+    console.log(`HTTPS Server is running on port: ${port}`);
+});
