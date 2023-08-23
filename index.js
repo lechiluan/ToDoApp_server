c2592ebe1797d69d4e01316e07a8c5bb90cd4c57
Lê Chí Luân (Brian Lee) <chiluan6601@gmail.com>
2023-08-23 22:42:46 +0700

    update server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
+const https = require('https'); // Import the 'https' module
+const fs = require('fs'); // Import the 'fs' module for reading SSL certificates
 const app = express();
 const cors = require('cors');
 const connection = require('./database');
@@ -19,22 +19,25 @@ app.use(express.json());
 app.use(cors());
 
 // routes
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 443;
+app.use("/", todoRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
-// Read SSL certificates
+// Load SSL certificate and key files
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/luanle.gcalls.vn/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/luanle.gcalls.vn/certificate.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/luanle.gcalls.vn/ca.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/luanle.gcalls.vn/fullchain.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/luanle.gcalls.vn/chain.pem', 'utf8'); 
 
 const credentials = {
-  key: privateKey,
-  cert: certificate,
-  ca: ca
+    key: privateKey,
+    cert: certificate,
+    ca: ca
 };
 
-// Create an HTTPS server
+// Create an HTTPS server with the app
 const httpsServer = https.createServer(credentials, app);
 
 httpsServer.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+    console.log(`Server is running on port: ${port}`);
 });
